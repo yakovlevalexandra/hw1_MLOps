1b8c8186ca2a99f2007626b0d003889aebb0e3bb
Alexandra <46657105+yakovlevalexandra@users.noreply.github.com>
2022-10-18 13:49:23 +0300

    Update ModelMaker.py

--- ModelMaker.py
+++ ModelMaker.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split, KFold, cross_validate, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -9,8 +9,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
 
-def prep_data() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    full_data = pd.read_csv('hotel_bookings_small.csv')
+def prep_data():
+    """
+    Preprocesses data for a model.
+    """
+
+    full_data = pd.read_csv('./data/hotel_bookings_small.csv')
     num_features = ["lead_time", "arrival_date_week_number", "arrival_date_day_of_month",
                     "stays_in_weekend_nights", "stays_in_week_nights", "adults", "children",
                     "babies", "is_repeated_guest", "previous_cancellations",
@@ -40,21 +44,70 @@ def prep_data() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]
 
 
 class ModelMaker:
+    """
+    A class to make a model.
+
+    ...
+
+    Attributes
+    ----------
+    model_key : int
+        an integer key for the model
+    model_type : str
+        type of the model ('RandomForestClassifier', 'LogisticRegression')
+
+    Methods
+    -------
+    fit(model_params):
+        Fits the model with the chosen parameters.
+    predict():
+        Returns predict of the model on test data.
+    get_params():
+        Gets all available parameters for the model.
+    """
+
     def __init__(self, model_key, model_type):
+        """
+        Makes all necessary attributes for the model object.
+
+        Parameters
+        ----------
+            model_key : int
+                 an integer key for the model
+            model_type : str
+                 type of the model ('RandomForestClassifier', 'LogisticRegression')
+        """
+
         models_dict = {'RandomForestClassifier': RandomForestClassifier(), 'LogisticRegression': LogisticRegression()}
         self.model_key = model_key
         self.model_type = model_type
         self.model = models_dict[self.model_type]
-        self.model_fitted = None
         self.x_train, self.x_test, self.y_train, self.y_test = prep_data()
 
     def fit(self, model_params):
+        """
+        Fits the model with the chosen parameters.
+
+        Parameters
+        ----------
+            model_params : dict
+                parameters for the model
+        """
+
         self.model.set_params(**model_params)
-        self.model_fitted = self.model.fit(self.x_train, self.y_train)
+        self.model.fit(self.x_train, self.y_train)
 
     def predict(self):
-        y_pred = self.model_fitted.predict(self.x_test)
+        """
+        Returns predict of the model on test data.
+        """
+
+        y_pred = self.model.predict(self.x_test)
         return y_pred
 
     def get_params(self):
+        """
+        Gets all available parameters for the model.
+        """
+
         return self.model.get_params()
